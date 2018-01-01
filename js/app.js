d31ede6ba3c25cc6e0127a1d35a3512c99a81c63
wendytamara <anacarlavegam@gmail.com>
2018-01-01 05:10:28 -0500

    añadiendo data.js

--- js/app.js
+++ js/app.js
@@ -6,6 +6,32 @@ function begin(){
      $('.section').show();
    },1000 );
 
+   var $container = $('#restaurants');
+
+   function showRestaurants() {
+     for (var i = 0; i < data.length; i++) {
+       var $containerFoods = $('<div></div>');
+       $containerFoods.addClass('col-xs-4 thumbnail');
+       $containerFoods.attr('data-name', data[i].name);
+       $containerFoods.attr('data-toggle', 'modal');
+       $containerFoods.attr('data-target', '#myModal');
+       $container.append($containerFoods);
+
+       var $text = $('<p></p>');
+       $text.addClass('title');
+       $text.$text(data[i].name);
+       containerFoods.append($text);
+
+
+
+
+
+
+
+     }
+
+   }
+
 
 
 
